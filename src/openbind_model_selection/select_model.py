a72e53690ea9528b42680b847354ba0c437fbd06
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2026-03-23 16:35:16 +0000

    First

--- src/openbind_model_selection/select_model.py
+++ src/openbind_model_selection/select_model.py
@@ -73,9 +73,10 @@ def select_model(sts: list[StructureModel]) -> StructureModel | None:
     """
     # Consider only structures with built ligands
     sts_with_ligands = {
-        {
-            pipeline: {x['path']: x for x in sts if (x['pipeline'] == pipeline) & (has_ligand(x))}
-            for pipeline in Pipeline}
+        pipeline: {x['path']: x for x in sts if (x['pipeline'] == pipeline) & (has_ligand(x))}
+        for pipeline 
+        in Pipeline
+        
     }
 
     if Pipeline.PanDDA2 in sts_with_ligands & Pipeline.Pipedream in sts_with_ligands:
